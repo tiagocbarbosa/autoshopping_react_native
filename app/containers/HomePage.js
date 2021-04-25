7ad1ec41ad83563b7a94347312f8a2edd213e72b
Tiago Castro Barbosa <tiago.c.barbosa@ericssoninovacao.com.br>
2021-04-25 19:50:30 -0300

    Structure changed

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,24 +1,10 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Provider as PaperProvider } from 'react-native-paper'
+import Homebar from '../components/appbars/Homebar'
 
 export default function App() {
-  return (
-    <PaperProvider>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </PaperProvider>
-  )
+    return (
+        <>
+            <Homebar />
+        </>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
